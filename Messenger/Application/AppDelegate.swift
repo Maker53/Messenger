ec8aa3c24b3d189766c148758bfe0499b6657087
Stanislav Shalgin <SShalgin@alfabank.ru>
2023-09-16 16:20:17 +0300

    Initial project settings

---
+++ Messenger/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//  Created by Stanislav Shalgin on 16.09.2023.
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
---
+++ Messenger/ViewController.swift
@@ -0,0 +1,11 @@
+//  Created by Stanislav Shalgin on 16.09.2023.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
