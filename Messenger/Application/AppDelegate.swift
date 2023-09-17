f447568515403e36478e298136e9c025a77a9b57
Stanislav Shalgin <SShalgin@alfabank.ru>
2023-09-17 12:02:35 +0300

    Add protocol to ViewController
    - implementing configure NavBar method

--- Messenger/Application/AppDelegate.swift
+++ Messenger/Application/AppDelegate.swift
@@ -4,11 +4,13 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let viewController = SettingsViewController()
+        let viewController: SettingsDisplayLogic = SettingsViewController()
+        viewController.configureNavigationBar()
+
         let navigationController = UINavigationController(rootViewController: viewController)
 
         window = UIWindow(frame: UIScreen.main.bounds)
--- Messenger/Modules/Settings/View/SettingsViewController.swift
+++ Messenger/Modules/Settings/View/SettingsViewController.swift
@@ -2,14 +2,40 @@
 
 import UIKit
 
+protocol SettingsDisplayLogic: UIViewController, AnyObject {
+    func configureNavigationBar()
+}
+
 final class SettingsViewController: UIViewController {
     // MARK: - Views
 
     private lazy var contentView: DisplaysSettingsView = SettingsView()
+    private lazy var leftBarButtonItem: UIBarButtonItem = .init(
+        image: UIImage(systemName: "qrcode"),
+        style: .plain,
+        target: nil,
+        action: nil
+    )
+
+    private lazy var rightBarButtonItem: UIBarButtonItem = .init(
+        title: "Edit",
+        style: .plain,
+        target: nil,
+        action: nil
+    )
 
     // MARK: - Lifecycle
 
     override func loadView() {
         view = contentView
+    }
+}
+
+// MARK: - SettingsDisplayLogic
+
+extension SettingsViewController: SettingsDisplayLogic {
+    func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+        navigationItem.leftBarButtonItem = leftBarButtonItem
     }
 }
